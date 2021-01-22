1ae2878469b93e608f99cf935c0302276a916653
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-01-22 12:09:29 +0530

    Use the resources routing convention for todos
    Signed-off-by: sam9111 <samyuktha19095@cse.ssn.edu.in>

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,25 +1,27 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string}.join("\n")
+    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
 
   def show
-    id=params[:id]
-    todo=Todo.find(id)
+    id = params[:id]
+    todo = Todo.find(id)
     render plain: todo.to_pleasant_string
   end
 
   def create
-    todo_text=params[:todo_text]
-    due_date=DateTime.parse(params[:due_date])
-    completed=params[:completed]
-    new_todo=Todo.create!(
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    completed = params[:completed]
+    new_todo = Todo.create!(
       todo_text: todo_text,
       due_date: due_date,
       completed: completed,
     )
-    response_text="Hey,your new todo is created with the id #{new_todo.id}!"
+    response_text = "Hey,your new todo is created with the id #{new_todo.id}!"
     render plain: response_text
   end
 end
