b8c0fdd2f7f7ba4ed231be0a866eb582ce9f7100
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-01-22 20:37:53 +0530

    Able to view all users in database by visiting /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def index
+    render plain: Users.order(:created_at).map{|user| user.to_pleasant_string}.join("\n")
   end
+
+end
--- app/models/users.rb
+++ app/models/users.rb
@@ -1,3 +1,5 @@
-class Todo < ActiveRecord::Base
-
+class Users < ActiveRecord::Base
+  def to_pleasant_string
+    "#{id}. #{name} #{email}"
+  end
 end
