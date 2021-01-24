ebbca37efee044555eb1ca02fd1a742654da3ef2
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-01-24 13:13:16 +0530

    Rendered dynamic HTML & CSS with ERB templating

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,36 @@
 class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
   def to_pleasant_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.to_displayable_list
+    all.map { |todo| todo.to_pleasant_string }
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+  def self.completed
+    all.where(completed: true)
+  end
+  def self.add_task(new_todo)
+    todo = Todo.new
+    todo.todo_text = new_todo[:todo_text]
+    todo.due_date = Date.today + new_todo[:due_in_days]
+    todo.completed = false
+    todo.save
+    todo
   end
 end
