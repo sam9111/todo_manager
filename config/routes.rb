5026180a76a9ae16c2af0bcc438ef5c93b86f8f4
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-01-24 16:52:09 +0530

    Updated root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get "todos", to: "todos#index"
   # post "todos", to: "todos#create"
   # get "todos/:id", to: "todos#show"
-
+  root 'todos#index'
   resources :todos
   resources :users
   post "/users/login", to: "users#login"
