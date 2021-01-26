b3174f69dd642fe75813f75109214c62d9685a85
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-01-26 15:30:20 +0530

    Raised flash error when user signs up without first name or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :todos
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 5 }
+  validates :first_name, presence: true
 
   def to_pleasant_string
     "User #{id}: #{name} #{email} #{password}"
